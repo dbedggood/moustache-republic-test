257ac95f214e7390395dab254e8bd62ec2304489
Daniel Bedggood <ddbedggood@gmail.com>
2019-08-09 16:52:14 +1200

    Dropdown menu

--- src/components/content/ProductForm.js
+++ src/components/content/ProductForm.js
@@ -1,25 +1,26 @@
-import React from 'react'
-
-function ProductForm() {
-  return (
-    <div>
-      <p className="lightText">
-        <b>
-          SIZE<span className="required">*</span>{' '}
-          <span className="Size">S</span>
-        </b>
-      </p>
-      <div className="lightText">
-        <button className="sizeButton">S</button>
-        <button className="sizeButton">M</button>
-        <button className="sizeButton">L</button>
-      </div>
-      <br />
+import React, { Component } from 'react'
+class ProductForm extends Component {
+  render() {
+    return (
       <div>
-        <button className="addToCartButton">ADD TO CART</button>
+        <p className="lightText">
+          <b>
+            SIZE<span className="required">*</span>{' '}
+            <span className="Size">S</span>
+          </b>
+        </p>
+        <div className="lightText">
+          <button className="sizeButton">S</button>
+          <button className="sizeButton">M</button>
+          <button className="sizeButton">L</button>
+        </div>
+        <br />
+        <div>
+          <button className="addToCartButton">ADD TO CART</button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default ProductForm
--- src/components/layout/ShoppingCart.js
+++ src/components/layout/ShoppingCart.js
@@ -3,9 +3,37 @@ import React from 'react'
 function ShoppingCart() {
   return (
     <div className="row">
-      <div className="col-2 offset-9">
-        <i className="fa fa-shopping-cart lightText d-lg-none" />
-        <p className="float-right lightText d-none d-lg-block">My Cart</p>
+      <div className="col-2 offset-9 dropdown">
+        <i className="fa fa-shopping-cart lightText d-md-none d-lg-none" />
+        <p className="float-right lightText d-none d-md-block d-lg-block">
+          My Cart
+        </p>
+        <div class="dropdown-content">
+          <div className="row clothesItem">
+            <div className="col-4">
+              <img src="./classic-tee.jpg" width="80" alt="tshirt" />
+            </div>
+            <div className="col-6">
+              <p>Classic Tee</p>
+              <p>
+                1x <b>$75.00</b>
+              </p>
+              Size: S
+            </div>
+          </div>
+          <div className="row clothesItem">
+            <div className="col-4">
+              <img src="./classic-tee.jpg" width="80" alt="tshirt" />
+            </div>
+            <div className="col-6">
+              <p>Classic Tee</p>
+              <p>
+                3x <b>$75.00</b>
+              </p>
+              Size: L
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
